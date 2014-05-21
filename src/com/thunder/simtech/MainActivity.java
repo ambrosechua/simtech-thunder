34e2f680a71f1202c581cba7d03bda5252c26680
Ambrose Chua <ambrose.chua.cm@gmail.com>
2014-05-21 15:13:35 +0800

    Before cleanup

--- src/com/thunder/simtech/MainActivity.java
+++ src/com/thunder/simtech/MainActivity.java
@@ -11,53 +11,34 @@ import android.webkit.WebViewClient;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
-    // The onCreate method is called when the Activity is created. Code to instantiate the UI should be placed here.
-	@Override
+    @Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		// This call tells the system to inflate the user interface defined in main.xml as the User Interface for this Activity.
-		setContentView(R.layout.activity_main); 
-
-        // The WebView needs to use a modified WebViewClient that does not delegate the URL to the default browser.
+		setContentView(R.layout.activity_main);
 		final WebView webView = (WebView) findViewById(R.id.webView);
 		webView.setWebViewClient(new WebViewClient() {
 			public boolean shouldOverrideUrlLoading(WebView view, String url){
 				view.loadUrl(url);
-				
-				// Gets the editText
 				EditText editText = (EditText) findViewById(R.id.editText);
-				// Updates the URL
 				editText.setText(webView.getUrl());
-				
-				// Hides the soft keyboard
 				editText.clearFocus();
 				InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 				imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
-
-				return false; // Tells the system not to continue propagating the event.
+				return false;
 			}
 		});
 
-		webView.loadUrl("http://beta.typega.me:4006/dl/");
+		webView.loadUrl("http://www.google.com/");
 		
 		// You can add other code here if you want.
+		
 	}
-    
-    // Tell the Button you create to call this method, by setting the android:onCreate attribute.
 	public void onGo(View view){
 		WebView webView = (WebView) findViewById(R.id.webView);
 		EditText editText = (EditText) findViewById(R.id.editText);
 
 		if (editText.getText()==null)return;
 		String url = editText.getText().toString();
-		
-//		if (url.equals("man man")) {
-//			Intent i = new Intent(this, AboutActivity.class);
-//		    startActivity(i);
-//			return;
-//		}
 		
 		if (!url.contains("http://"))url = "http://"+url;
 		webView.loadUrl(url);
