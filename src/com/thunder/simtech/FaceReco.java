2eec50b8a25a109d9dea522d1212bfb34d84e4bb
Hanpu Liu <spamlhp@gmail.com>
2014-05-21 22:47:00 +0800

    Added FaceReco Java Class for the decrypting
    New class added( amb pls find a way to pull data from webview api)

---
+++ src/com/thunder/simtech/FaceReco.java
@@ -0,0 +1,12 @@
+package com.thunder.simtech;
+
+/**
+ * Created by Hanpu on 5/21/14. This part will be used for receiving the data from webview api and retrieving a password and decrypting
+ * it using something to do with the facial recognition api. Then push resulting password back into webview api text field on page
+ * using Javascript
+ */
+public class FaceReco {
+    /*
+    Ambrose pls find someway to pull the required info from webview api
+     */
+}
