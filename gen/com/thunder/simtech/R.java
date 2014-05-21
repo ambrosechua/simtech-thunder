a7b4e7392f081489fa3713f319f39ff19b2ecea3
Ambrose Chua <ambrose.chua.cm@gmail.com>
2014-05-21 16:13:46 +0800

    yay

--- gen/com/thunder/simtech/R.java
+++ gen/com/thunder/simtech/R.java
@@ -21,19 +21,16 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ab_bottom_solid_dark_holo=0x7f020000;
+        public static final int ab_solid_dark_holo=0x7f020000;
         public static final int ic_launcher=0x7f020001;
         public static final int ic_menu_forward=0x7f020002;
     }
     public static final class id {
-        public static final int editText=0x7f080000;
-        public static final int webView=0x7f080001;
+        public static final int editText=0x7f070000;
+        public static final int webView=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-    }
-    public static final class menu {
-        public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
@@ -48,17 +45,6 @@ public final class R {
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
